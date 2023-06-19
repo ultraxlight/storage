d9f9a9b82f49cf78d33b6ea427dfdf350fd2c56a
Dan Woodson <woodson.dan@gmail.com>
2023-06-18 22:22:16 -0600

    Basic docs #7

--- src/types.ts
+++ src/types.ts
@@ -11,14 +11,32 @@ export type Create = (
   partialItem?: Record<string, unknown>,
 ) => { id: string; [key: string]: unknown }
 
+/**
+ * Get a single Item from storage
+ * @param {string} id ID of item to retrieve
+ * @returns {null|Item} Item or null
+ */
 export interface Get {
   <Schema extends Item>(id: string): null | Schema
 }
 
+/**
+ * Get all Items from storage
+ * @returns {Item[]} Items
+ */
 export interface Get {
   <Schema extends Item>(): Schema[]
 }
 
+/**
+ * Remove an item from storage
+ * @param {string} id ID of item to remove
+ * @returns {Item|null}
+ */
 export type Remove = <Schema extends Item>(id: string) => Schema | null
 
+/**
+ * Remove ALL items
+ * @returns {Item[]}
+ */
 export type RemoveAll = <Schema extends Item>() => Schema[] | null
